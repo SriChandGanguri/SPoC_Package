959bac56d5350b49bfba926b325f35446a32f68d
Sri Chand Ganguri <srichand@uvik.in>
2023-01-05 13:55:34 +0530

    Initial Commit

--- Package.swift
+++ Package.swift
@@ -8,21 +8,33 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SPoC_Package",
-            targets: ["SPoC_Package"]),
+            name: "SPoC",
+            targets: ["SPoCTarget"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/Moya/Moya", exact: "15.0.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire", exact: "5.6.4")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "SPoC_Package",
-            dependencies: []),
-        .testTarget(
-            name: "SPoC_PackageTests",
-            dependencies: ["SPoC_Package"]),
+            name: "SPoCTarget",
+            dependencies: [.target(name: "SPoCWrapper")]
+        ),
+        .target(
+            name: "SPoCWrapper",
+            dependencies: [
+                .target(name: "SPoC"),
+                .product(name: "Moya", package: "Moya"),
+                .product(name: "Alamofire", package: "Alamofire")
+            ]
+            
+        ),
+        .binaryTarget(
+            name: "SPoC",
+            path: "./Sources/SPoC/SPoC.xcframework")
     ]
 )
---
+++ Sources/SPoC/SPoC.xcframework/ios-arm64_x86_64-simulator/SPoC.framework/Headers/SPoC.h
@@ -0,0 +1,18 @@
+//
+//  SPoC.h
+//  SPoC
+//
+//  Created by Sri Chand Ganguri on 05/01/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SPoC.
+FOUNDATION_EXPORT double SPoCVersionNumber;
+
+//! Project version string for SPoC.
+FOUNDATION_EXPORT const unsigned char SPoCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SPoC/PublicHeader.h>
+
+
